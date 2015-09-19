00e7de3df8e69448574a6cc7a65cd0a66926fd92
Bill Skrzypczak <bill@webbuildersconsulting.com>
2015-09-19 10:19:47 -0500

    Initial commit

---
+++ afl/src/main/java/com/example/MyClass.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class MyClass {
+
+    public static void main(String args[]) {
+
+        for(int x = 10; x < 20; x = x+1) {
+            System.out.print("value of x : " + x );
+            System.out.print("\n");
+        }
+    }
+
+
+}
